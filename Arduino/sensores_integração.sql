5a0ce19aa5fffc85f8c6ec6933f253528c25d16e
MichaMendes <michelly.silva@sptech.school>
2023-10-12 22:03:35 -0300

    Ajuste tela de cadastro

--- Arduino/sensores_integração.sql
+++ Arduino/sensores_integração.sql
@@ -1,10 +1,9 @@
-create database if not exists metricas;
+create database metricas;
 
 use metricas;
 
-create table if not exists sensores (
-id int PRIMARY KEY AUTO_INCREMENT,
-dtHora DATETIME default current_timestamp NOT NULL,
+create table sensores (
+idMetricas int primary key auto_increment,
 dht11_umidade float,
 dht11_temperatura float,
 luminosidade float,
@@ -12,5 +11,7 @@ lm35_temperatura float,
 chave int
 );
 
-SELECT * FROM sensores;
+select * from sensores;
+
+
 
